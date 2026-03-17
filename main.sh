7e3923bc73e80bac6dc2e4182c244e142fecb911
Devobass <devobass@disroot.org>
2026-03-17 09:50:57 +0700

    init

---
+++ main.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env sh
+
+if [ "$(id -u)" -ne 0 ]
+then
+	printf "Not running as root, try again.\n"
+	exit 1
+fi
+
+close() {
+	printf "Closing LUKS.\n"
+
+	umount "/mnt/$LUKS_UUID"
+	cryptsetup luksClose "BACKUP_$LUKS_UUID"
+	umount "/media/$USB_UUID"
+}
+
+mount_drives() {
+	if mountpoint -q "/media"
+	then
+		printf "Please unmount /media before continuing.\n"
+		exit 1
+	fi
+
+	if mountpoint -q "/mnt"
+	then
+		printf "Please unmount /mnt before continuing.\n"
+		exit 1
+	fi
+
+	mkdir -p "/media/$USB_UUID"
+	mkdir -p "/mnt/$LUKS_UUID"
+
+	mount "/dev/disk/by-uuid/$USB_UUID" "/media/$USB_UUID"
+}
+
+verify() {
+	KEYFILE="/media/$USB_UUID/$LUKS_UUID.key"
+	CHECKSUM="$LUKS_UUID.b3sum"
+
+	cd "/media/$USB_UUID"
+	if ! b3sum -c "$CHECKSUM"
+	then
+		printf "WARNING: CHECKSUM MISMATCH, YOUR KEY MIGHT BE CORRUPTED!\n"
+		exit 1
+	fi
+}
+
+open_luks() {
+	cryptsetup luksOpen --key-file "/media/$USB_UUID/$LUKS_UUID.key" "/dev/disk/by-uuid/$LUKS_UUID" "BACKUP_$LUKS_UUID"
+	mount "/dev/mapper/BACKUP_$LUKS_UUID" "/mnt/$LUKS_UUID"
+}
+
+init() {
+	rm -f ./config.sh
+	printf "Select LUKS drive.\n"
+	printf "Accepted input: /dev/sdX, /dev/nvmeXnYpZ.\n"
+	blkid --output device
+	read device
+
+	LUKS_UUID="$(blkid -s UUID -o value "$device")"
+	
+	printf "Select drive that has the keyfile.\n"
+	printf "Accepted input: /dev/sdX, /dev/nvmeXnYpZ.\n"
+	blkid --output device
+	read device
+
+	USB_UUID="$(blkid -s UUID -o value "$device")"
+
+	printf "LUKS_UUID=\"$LUKS_UUID\"\n" >> "./config.sh"
+	printf "USB_UUID=\"$USB_UUID\"\n" >> "./config.sh"
+}
+
+open() {
+	mount_drives
+	verify
+	open_luks
+	printf "Drive is open at /mnt/%s\n" "$LUKS_UUID"
+}
+
+help() {
+	printf "Usage: main.sh open|close|init\n"	
+	printf "open: open luks\n"	
+	printf "close: close luks\n"	
+	printf "init: create/change config\n"	
+	exit 0
+}
+
+if [ ! -r "./config.sh" ]
+then
+	printf "config.sh not found, initializing.\n"
+	init
+fi
+
+. "./config.sh"
+
+if [ -z "$1" ] 
+then
+	help
+fi
+
+case "$1" in
+	open) open ;;
+	close) close ;;
+	init) init ;;
+	*) help ;;
+esac
---
+++ make_luks.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env sh
+
+if [ "$(id -u)" -ne 0 ]
+then
+	printf "Not running as root, try again.\n"
+	exit 1
+fi
+
+close() {
+	umount "/media/$USB_UUID"
+}
+trap close EXIT
+
+mount_usb() {
+	if mountpoint -q "/media" 
+	then
+		printf "Please unmount /media before continuing.\n"
+		exit 1
+	fi
+
+	mkdir -p "/media/$USB_UUID"
+	mount --uuid "$USB_UUID" "/media/$USB_UUID"
+}
+
+verify() {
+	cd "/media/$USB_UUID"
+
+	if [ -e "$LUKS_UUID.b3sum" ]
+	then
+		printf "Checksum file exists, stopping."
+		exit 1
+	fi
+	
+	b3sum "$LUKS_UUID.key" > "$LUKS_UUID.b3sum"
+	b3sum "$LUKS_UUID.header" >> "$LUKS_UUID.b3sum"
+}
+
+create_luks() {
+	if [ "$(blkid "/dev/disk/by-uuid/$LUKS_UUID" -s TYPE -o value)" = "crypto_LUKS" ]
+	then
+		printf "LUKS detected, stopping."
+		exit 1
+	fi
+	cryptsetup luksFormat --type luks2 "/dev/disk/by-uuid/$LUKS_UUID"
+}
+
+backup_header() {
+	cryptsetup luksHeaderBackup "/dev/disk/by-uuid/$LUKS_UUID" --header-backup-file "/media/$USB_UUID/$LUKS_UUID.header"
+}
+
+add_keyfile() {
+	if [ -e "/media/$USB_UUID/$LUKS_UUID.key" ]
+	then
+		printf "Key exists, exiting."
+		exit 1
+	fi
+	dd if="/dev/urandom" of="/media/$USB_UUID/$LUKS_UUID.key" bs=4096 count=1	
+	cryptsetup luksAddKey "/dev/disk/by-uuid/$LUKS_UUID" "/media/$USB_UUID/$LUKS_UUID.key"
+}
+
+init() {
+	rm -f ./config.sh
+	printf "Select LUKS drive.\n"
+	printf "Accepted input: /dev/sdX, /dev/nvmeXnYpZ.\n"
+	blkid --output device
+	read device
+
+	LUKS_UUID="$(blkid -s UUID -o value "$device")"
+	
+	printf "Select drive that will store the keyfile.\n"
+	printf "Accepted input: /dev/sdX, /dev/nvmeXnYpZ.\n"
+	blkid --output device
+	read device
+
+	USB_UUID="$(blkid -s UUID -o value "$device")"
+
+	printf "LUKS_UUID=\"$LUKS_UUID\"\n" >> "./config.sh"
+	printf "USB_UUID=\"$USB_UUID\"\n" >> "./config.sh"
+}
+
+if [ ! -r "./config.sh" ]
+then
+	printf "config.sh not found, initializing.\n"
+	init
+fi
+
+. "./config.sh"
+
+mount_usb
+create_luks
+add_keyfile
+backup_header
+verify
+printf "Completed.\n"
